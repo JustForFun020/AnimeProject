a94c640b9fda46072451c4aea79c123f80d2ee48
Your Name <you@example.com>
2022-02-22 15:13:55 +0700

    update

--- src/component/Anime/Items.js
+++ src/component/Anime/Items.js
@@ -1,21 +1,20 @@
-
-const Items = ({characters, filterItem}) => {
-    return (
-        <div className="all__btn">
-            {characters.map((character, index) => {
-                return (
-                    <button
-                        key={index}
-                        type="button"
-                        className="btn"
-                        onClick={() => filterItem(character)}
-                    >
-                        {character}
-                    </button>
-                )
-            })}
-        </div>
-    )
-}
+const Items = ({ characters, filterItem }) => {
+  return (
+    <div className="all__btn">
+      {characters.map((character, index) => {
+        return (
+          <button
+            key={index}
+            type="button"
+            className="btn"
+            onClick={() => filterItem(character)}
+          >
+            {character}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Items;
